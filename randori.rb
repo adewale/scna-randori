1dbd0d30737de93b3207cc2760a2afc236e63987
Ade Oshineye <adewale@gmail.com>
2010-10-16 20:16:42 -0500

    Added description of exercise from SCNA 2010 session: Randori With The Stars

---
+++ randori.rb
@@ -0,0 +1,19 @@
+require "rubygems"
+gem "rspec"
+
+# People
+# | Person | Pin  | Balance |
+# | Alice  | 7643 | 2000    |
+# | Bob    | 5954 | 22400   |  
+# | Clyde  | 1562 | 100     |
+
+# Stories to work through
+
+# Entering a PIN
+# Checking a PIN for numerical validity
+# Reject invalid PIN
+# Allowing three attempts to get the PIN correct
+# Withdrawing cash (debiting both the customer’s account and the amount of money within the ATM itself)
+# If you have time then model the ability to do either or both of the following after withdrawing cash:
+# Display the remaining account balance
+# Print a receipt
